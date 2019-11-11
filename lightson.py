fa5a4fc59e4a7305e00ff8c1270a935fecb9fc42
Bruce Lowekamp <bruce@lowekamp.net>
2019-11-11 10:56:31 -0500

    first pass of lights

---
+++ lightson.py
@@ -0,0 +1,40 @@
+import array
+from ola.ClientWrapper import ClientWrapper
+import math
+
+wrapper = None
+
+def DmxSent(state):
+  if not state.Succeeded():
+    wrapper.Stop()
+
+
+wrapper = ClientWrapper()
+
+# compute frame here
+data = array.array('B')
+
+# for i in range(100):
+#   light = array.array('B', [0, 0, 0])
+#   light[i%3] = 255
+#   data.extend(light)
+
+for i in range(300):
+  data.append(0)
+
+LENGTH=100
+NUMSETS=10
+SETSIZE = int(LENGTH/NUMSETS)
+
+for set in range(NUMSETS):
+  base = set * SETSIZE*3
+  color = set % 3
+  for pixel in range(SETSIZE):
+    #data[base+pixel*3+color] = int((pixel+1)/SETSIZE*255)
+    print (int(math.exp((pixel+1)/SETSIZE*6-6)*255))
+    data[base+pixel*3+color] = int(math.exp((pixel+1)/SETSIZE*6-6)*255)
+
+wrapper.Client().SendDmx(1, data, DmxSent)
+
+
+wrapper.Run()
