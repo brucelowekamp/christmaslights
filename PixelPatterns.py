fbdf76b9654af39780195b9b1353174098c26d6e
Bruce Lowekamp <bruce@lowekamp.net>
2019-12-01 15:26:30 -0500

    heather strand with non-reversing sparkler

--- PixelPatterns.py
+++ PixelPatterns.py
@@ -13,7 +13,7 @@ class PixelPatterns(object):
     parser.add_argument('--fademin', type=int, default=6, help="min side of exp in rgfade")
     parser.add_argument('--fadesets', type=int, default=10, help="number of fadesets per strand in rgfade")
     parser.add_argument('--rgsize', type=int, default=5, help="size of red/green blocks in RG pattern")
-    parser.add_argument('--sparkfrac', type=float, default=0.008, help="proportion of pixels sparkling")
+    parser.add_argument('--sparkfrac', type=float, default=0.01, help="proportion of pixels sparkling")
     parser.add_argument('--flowfrac', type=float, default=0.5, help="proportion of pixels in flow")
     parser.add_argument('--sparksteps', type=int, default=2, help="steps up then down in spark")
     parser.add_argument('--flowsteps', type=float, default=15, help="steps up then down in flow")
@@ -24,6 +24,10 @@ class PixelPatterns(object):
     PixelPatterns.options = options
 
   @staticmethod
+  def WhiteColor():
+    return (255, 255, 255)
+    
+  @staticmethod
   def RedGreen(display):
     print ("RedGreen pattern")
     i = 0
@@ -31,7 +35,7 @@ class PixelPatterns(object):
       red = (i//PixelPatterns.options.rgsize)%2 == 0
       display.ColorSet(p, 255 if red else 0, 0 if red else 255, 0)
       i+=1
-    return Sparkler(display, 255, 255, 255, PixelPatterns.options.sparksteps, PixelPatterns.options.sparkfrac)
+    return Sparkler(display, PixelPatterns.WhiteColor, PixelPatterns.options.sparksteps, PixelPatterns.options.sparkfrac)
   
   @staticmethod
   def RGFade(display):
@@ -59,12 +63,12 @@ class PixelPatterns(object):
     return None
 
   @staticmethod
-  def White(display):
-    print ("White")
+  def WhiteFlow(display):
+    print ("WhiteFlow")
     for p in display:
       display.ColorSet(p, 175, 175, 100)
 
-    return Sparkler(display, 0, 0, 255, PixelPatterns.options.flowsteps, PixelPatterns.options.flowfrac)
+    return Sparkler(display, lambda: (0, 0, 255), PixelPatterns.options.flowsteps, PixelPatterns.options.flowfrac)
 
   # what we want to do is change the spectral rainbow to look more
   # evenly distributed colorwise to what humans think rainbows look like
@@ -109,13 +113,35 @@ class PixelPatterns(object):
         x = PixelPatterns.FracToRainbow(x)
         (r, g, b) = wavelength_to_rgb(x*370+380)
         s.ColorSet(p, r, g, b)
-    return Sparkler(display, 255, 255, 255, PixelPatterns.options.sparksteps, PixelPatterns.options.sparkfrac)
+    return Sparkler(display, PixelPatterns.WhiteColor, PixelPatterns.options.sparksteps, PixelPatterns.options.sparkfrac)
 
+  # heather wants a strand of red, green, blue, white, and purple
+  HeathersColors = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (200, 200, 200),
+    (220, 0, 220)
+  ]
+  @staticmethod
+  def PickHeatherColor():
+    return random.choice(PixelPatterns.HeathersColors)
+  
+  @staticmethod
+  def HeatherStrand(display):
+    print ("RGBWP pattern")
+    for p in display:
+      color = PixelPatterns.PickHeatherColor()
+      display.ColorSet(p, color[0], color[1], color[2])
+    return Sparkler(display, PixelPatterns.PickHeatherColor,
+                    PixelPatterns.options.flowsteps, PixelPatterns.options.flowfrac,
+                    reverse = False)
+  
   @staticmethod
   def RGFlow(display):
     print ("RGFlow")
     for p in display:
       display.ColorSet(p, 255, 0, 0)
-    return Sparkler(display, 0, 255, 0, PixelPatterns.options.flowsteps, PixelPatterns.options.flowfrac)
+    return Sparkler(display, lambda: (0, 255, 0), PixelPatterns.options.flowsteps, PixelPatterns.options.flowfrac)
 
-PixelPatterns.Patterns = [PixelPatterns.RedGreen, PixelPatterns.RGFade, PixelPatterns.White, PixelPatterns.Rainbow, PixelPatterns.RGFlow]
+PixelPatterns.Patterns = [PixelPatterns.RedGreen, PixelPatterns.HeatherStrand, PixelPatterns.WhiteFlow, PixelPatterns.Rainbow, PixelPatterns.RGFlow]
--- Sparkler.py
+++ Sparkler.py
@@ -4,44 +4,64 @@ class Sparkler(object):
   @staticmethod
   def SetArgs(parser):
     pass
-  
-  def __init__(self, display, red, green, blue, steps=4, fraction=0.02):
+
+  # sparkler is used to twinkle (flash white), to flow (slowly change to a color and back)
+  # and to permanently change to a new color
+  # display is the overall PixelDisplay
+  # colorfunc returns the color to change to
+  # reverse indicates to go back
+  # steps is how long to take (in each direction for reversing functions)
+  # fraction is fraction of pixels in display to be changing at any one time
+  def __init__(self, display, colorfunc, steps=4, fraction=0.02, reverse =  True):
     self._display = display
     #self._options = options
-    self._Target = [red, green, blue]
+    self._Colorfunc = colorfunc
     self._Steps = steps
+    self._reverse = reverse
     self._NumFlashers = max(int(fraction * self._display.length), 1)
     self._flashers = []
     self._being_flashed = set([])
 
   class Flasher(object):
-    def __init__(self, display, pixel, target, steps):
+    def __init__(self, display, pixel, colorfunc, steps, reverse):
       self._display = display
       self._pixel = pixel
       self._onstep = int(steps)
       self._orig = self._display.ColorGet(pixel)
-      self._curcolor = list(self._orig) #copy
+      self._curcolor = list(self._orig) # copy
       self._steps = steps
+      self._reverse = reverse
+      self._done = False
       self._delta = [0, 0, 0]
+      target = colorfunc()
       for i in xrange(3):
         self._delta[i] = (target[i] - self._orig[i])/(steps*1.0)
 
+    @property
+    def done(self):
+      return self._done
+        
     def flash(self):
       self._onstep -= 1
-      if (self._onstep > -1 * self._steps):
+      if ( ( self._reverse and (self._onstep > -1 * self._steps))
+             or
+          ( not self._reverse and (self._onstep >= 0))):
         for i in xrange(3):
           if self._onstep >= 0:
             self._curcolor[i] = self._curcolor[i] + self._delta[i]
           else:
             self._curcolor[i] = self._curcolor[i] - self._delta[i]
 
+          # in case rounding makes us a bit off, set to limits
           if (int(self._curcolor[i]) < 0):
             self._curcolor[i] = 0
           elif (int(self._curcolor[i]) > 255):
             self._curcolor[i] = 255
       else:
-        self._curcolor = self._orig
-      #print "step ", self._onstep, " colors", self._curcolor
+        if (self._reverse): self._curcolor = self._orig
+        self._done = True
+
+      #print "step ", self._onstep, "pixel", self._pixel[1], "colors", self._curcolor
       self._display.ColorSet(self._pixel, int(self._curcolor[0]), int(self._curcolor[1]), int(self._curcolor[2]))
 
 
@@ -52,7 +72,7 @@ class Sparkler(object):
         break
     self._being_flashed.add(pixel)
     self._flashers.append(Sparkler.Flasher(self._display, pixel,
-                                           self._Target, self._Steps))
+                                           self._Colorfunc, self._Steps, self._reverse))
     
     
   def Sparkle(self):
@@ -61,7 +81,7 @@ class Sparkler(object):
 
     for flasher in self._flashers:
       flasher.flash()
-    finished = filter(lambda f: f._onstep <= -1 * self._Steps, self._flashers)
+    finished = filter(lambda f: f.done, self._flashers)
     for f in finished:
       self._flashers.remove(f)
       self._being_flashed.remove(f._pixel)
