0d79255cc8e6b14e2842c99a1aa365636fb3913c
Bruce Lowekamp <bruce@lowekamp.net>
2019-12-07 13:04:59 -0500

    non-grinch simple show

---
+++ RunSimpleShow.py
@@ -0,0 +1,54 @@
+# run overall system without grinch
+
+from ola.ClientWrapper import ClientWrapper
+from Relays import Relays
+from Show import Show
+from Options import Options
+import os
+import argparse
+import time
+
+Options.ParseArgs()
+
+# kill any currently running shows
+os.system('killall --older-than 1m python')
+
+
+wrapper = ClientWrapper()
+relays = Relays(wrapper)
+
+# turn off 12v (on NC so sense is reversed)
+# (conincidentally turns off everything else, too)
+# (should already be off)
+relays.on(Show.Relays.POWER)
+relays.SendDmx()
+
+time.sleep(2)
+
+# restart olad
+os.system('sudo /etc/init.d/olad restart')
+
+time.sleep(5)
+
+wrapper = ClientWrapper()
+relays = Relays(wrapper)
+
+relays.off(Show.Relays.POWER)
+relays.SendDmx()
+
+time.sleep(5)
+
+# run program in loop
+while True:
+  os.system('python SimpleShow.py')
+  print "SHOW LOOPING!!!!!"
+  time.sleep(1)
+
+  # run this reset every time b/c if the code exits there is a reason
+  os.system('sudo /etc/init.d/olad restart')
+
+  time.sleep(5)
+
+
+
+
