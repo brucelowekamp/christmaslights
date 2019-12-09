315962f280aa81f52de2833074e25c0b19c827df
Bruce Lowekamp <bruce@lowekamp.net>
2019-12-09 10:29:06 -0500

    single strand test for current measurement

---
+++ AmpTest.py
@@ -0,0 +1,39 @@
+# run overall system without grinch
+
+from ola.ClientWrapper import ClientWrapper
+from Relays import Relays
+from Show import Show
+from Options import Options
+import array
+import os
+import argparse
+import time
+
+def DmxSent(state):
+  if not state.Succeeded():
+    print ("Error: ", state.message)
+    raise
+
+
+Options.ParseArgs()
+
+wrapper = ClientWrapper()
+relays = Relays(wrapper)
+
+# turn on 12v (on NC so sense is reversed)
+# (conincidentally turns off everything else, too)
+# (should already be off)
+print "12v on"
+relays.off(Show.Relays.POWER)
+relays.SendDmx()
+
+time.sleep(8)
+
+length = 50
+pixels = array.array('B', [255] * length*3)
+
+wrapper.Client().SendDmx(2, pixels, DmxSent)
+print "white sent 50 pixels to univ 2"
+time.sleep(2)
+wrapper.Run()
+
