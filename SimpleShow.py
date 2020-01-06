231ec47432ef5140ecd27b1c62e2edff8e2249b2
Bruce Lowekamp <bruce@lowekamp.net>
2020-01-05 21:01:32 -0500

    support simple with grinches on

--- SimpleShow.py
+++ SimpleShow.py
@@ -13,21 +13,23 @@ class SimpleShow(Show):
   def SetArgs(parser):
     parser.add_argument('--holdtime', type=int, default=60, 
                         help="time before changing pattern")
+    parser.add_argument('--grincheson', action="store_true",
+                        help="turn on even grinch and sleigh")
 
   def ReStart(self):
     super(SimpleShow, self).ReStart()
-
+    grinches = Show.Commands.OFF if not Options.grincheson else Show.Commands.ON
     self.LoadTimings( [
       #starting state
       (0, Show.Commands.ON, Show.Relays.OLAF_FAN),
-      (0, Show.Commands.OFF, Show.Relays.GRINCH_FAN),
-      (0, Show.Commands.OFF, Show.Relays.GRINCH_SLEIGH_FAN),
+      (0, grinches, Show.Relays.GRINCH_FAN),
+      (0, grinches, Show.Relays.GRINCH_SLEIGH_FAN),
       (0, Show.Commands.ON, Show.Relays.OLAF),
       (0, Show.Commands.ON, Show.Relays.LASER_PROJ),
       (0, Show.Commands.ON, Show.Relays.REINDEER),
       (0, Show.Commands.ON, Show.Relays.SNOWMAN),
-      (0, Show.Commands.OFF, Show.Relays.GRINCH_SLEIGH),
-      (0, Show.Commands.OFF, Show.Relays.GRINCH),
+      (0, grinches, Show.Relays.GRINCH_SLEIGH),
+      (0, grinches, Show.Relays.GRINCH),
 
       # pixels are on
       (0, Show.Commands.PIXELS_ON),
