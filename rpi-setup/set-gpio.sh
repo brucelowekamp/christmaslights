6a36419744057c3207a878b0964af940fc32da35
Bruce Lowekamp <bruce@lowekamp.net>
2020-11-28 16:30:18 -0500

    more 2020 pixel updates and update all rpi config

--- rpi-setup/set-gpio.sh
+++ rpi-setup/set-gpio.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-gpio export 21 low
-gpio export 20 low
+gpio export 11 low
+gpio export 12 low
+gpio export 13 low
+gpio export 14 low
+gpio export 15 low
 gpio export 16 low
+gpio export 17 low
+gpio export 18 low
+gpio export 19 low
+gpio export 20 low
+gpio export 21 low
+gpio export 22 low
+gpio export 2 low
 
