a2b02f3c696399126b5f6f4789a3759dd1548c14
Bruce Lowekamp <bruce@lowekamp.net>
2022-12-18 16:21:17 -0500

    turn off all relays without ola

--- KillShow.py
+++ KillShow.py
@@ -1,19 +1,13 @@
 # kill running shows (really just other python scripts)
 
-from ola.ClientWrapper import ClientWrapper
-from Relays import Relays
-from Show import Show
-from Options import Options
-
 import os
-import time
-
-Options.ParseArgs()
 
 # kill any currently running shows
 os.system('killall -r --older-than 5s python?')
 
 # turn off 12v (on NC so sense is reversed)
-# (conincidentally turns off everything else, too)
-# turn power supply on 
 os.system('gpio -g write 2 1')
+
+# turn off all other relays
+for relay in range(11,23):
+    os.system(f"gpio -g write {relay} 0")
